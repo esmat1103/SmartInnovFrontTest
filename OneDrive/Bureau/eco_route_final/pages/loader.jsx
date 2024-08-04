f64bddf7e08e146781e399c92b1d47663adfe16d
SIwar Melliti <160419246+SIWAR245@users.noreply.github.com>
2024-08-05 00:34:42 +0100

    Delete OneDrive/Bureau directory

--- OneDrive/Bureau/eco_route_final/pages/loader.jsx
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-  return (
-    <div>
-      <h1>My Next.js App</h1>
-      <div className={styles.loader}></div>
-    </div>
-  );
-}
--- OneDrive/Bureau/front/app/utils/getTranslations.js
+++
@@ -1,5 +0,0 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
-export const getTranslations = async (locale, namespaces) => {
-  return await serverSideTranslations(locale, namespaces);
-};
